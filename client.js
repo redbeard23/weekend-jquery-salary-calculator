6fb82c2b104d98cd04ab42056016c56a82a9a3ee
Pace Maier <pace.a.maier@gmail.com>
2021-04-24 09:41:14 -0500

    created employee array

--- client.js
+++ client.js
@@ -1,1 +1,31 @@
 console.log('is working!');
+//first step, create employees as objects
+
+const employees = [
+    
+    {
+    firstName: 'Jen',
+    lastName: 'Barber',
+    id: 4521,
+    title: 'Team Lead',
+    annualSalary: 80000
+    },
+
+    {
+    firstName: 'Maurice',
+    lastName: 'Moss',
+    id: 8724,
+    title: 'Support Team',
+    annualSalary: 58000
+    },
+
+    {
+    firstName: 'Roy',
+    lastName: 'Smith',
+    id: 9623,
+    title: 'Quality Assurance',
+    annualSalary: 48000
+    },
+
+]
+
